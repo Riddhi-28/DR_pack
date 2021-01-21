b25d7d68bd632a8243d97f753c3a161e78825a47
Riddhi-28 <77779492+Riddhi-28@users.noreply.github.com>
2021-01-21 16:24:13 +0530

    Add files via upload

---
+++ _init_.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 12:36:17 2021
+
+@author: MILIND
+"""
+ 
+from DR_pack.module1 import *
+from DR_pack.module3 import *
---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 21 15:03:40 2021
+
+@author: MILIND
+"""
+
+from setuptools import setup
+
+setup(
+   name='DR_pack',
+   version= version['__version__'],
+   description='A useful module',
+   author='Riddhi',
+   author_email='package@gmail.com',
+   packages=['package'])
